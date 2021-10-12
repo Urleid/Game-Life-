635ff2edc265809c212161a1d54778a6e1c80d2c
Urleid <79658234472@yandex.ru>
2021-10-12 14:49:49 +0700

    Создал систему отношений между персонажами

---
+++ Urleid.Remember/Actions.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urleid.Remember
+{
+    class Actions
+    {
+        /// <summary>
+        /// Знакомство персонажей.
+        /// </summary>
+        /// <param name="person1">Первый персонаж.</param>
+        /// <param name="person2">Второй персонаж.</param>
+        public static void Acquaintance(Person person1, Person person2)
+        {
+            if (person1.Relationship.ContainsKey(person2))
+            {
+                Console.WriteLine($"{person1.FirstName} и {person2.FirstName} уже знакомы");
+            }
+            else
+            {
+                BuildingRelationships(person1, person2, 0);
+                Console.WriteLine($"{person1.FirstName} и {person2.FirstName} теперь знакомые");
+            }
+        }
+
+        /// <summary>
+        /// Возникновение дружбы.
+        /// </summary>
+        /// <param name="person1">Первый персонаж.</param>
+        /// <param name="person2">Второй персонаж.</param>
+        public static void Friendship(Person person1, Person person2)
+        {
+            uint relationshipLevel;
+
+            if (!person1.Relationship.ContainsKey(person2))
+            {
+                Console.WriteLine($"{person1.FirstName} и {person2.FirstName} не знакомы");
+            }
+            else
+            {
+                person1.Relationship.TryGetValue(person2, out relationshipLevel);
+
+                if (relationshipLevel == 1) // Уровень отношений - друзья.
+                {
+                    Console.WriteLine($"{person1.FirstName} и {person2.FirstName} уже друзья");
+                }
+                else
+                {
+                    EditRelationships(person1, person2, 1);
+                    Console.WriteLine($"{person1.FirstName} и {person2.FirstName} теперь друзья");
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Возникновение любовных отношений.
+        /// </summary>
+        /// <param name="person1">Первый персонаж.</param>
+        /// <param name="person2">Второй персонаж.</param>
+        public static void LoveRelationship(Person person1, Person person2)
+        {
+            uint relationshipLevel;
+
+            if (!person1.Relationship.ContainsKey(person2))
+            {
+                Console.WriteLine($"{person1.FirstName} и {person2.FirstName} не знакомы");
+            }
+            else
+            {
+                person1.Relationship.TryGetValue(person2, out relationshipLevel);
+
+                if (relationshipLevel == 2) // Уровень отношений - любовники.
+                {
+                    Console.WriteLine($"{person1.FirstName} и {person2.FirstName} уже любовники");
+                }
+                else
+                {
+                    EditRelationships(person1, person2, 2);
+                    Console.WriteLine($"Между {person1.FirstName} и {person2.FirstName} возникают любовные отношения");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создание семьи.
+        /// </summary>
+        /// <param name="person1">Первый персонаж.</param>
+        /// <param name="person2">Воорой персонаж.</param>
+        public static void Wedding(Person person1, Person person2)
+        {
+            uint relationshipLevel;
+
+            if (!person1.Relationship.ContainsKey(person2))
+            {
+                Console.WriteLine($"{person1.FirstName} и {person2.FirstName} не знакомы");
+            }
+            else
+            {
+                person1.Relationship.TryGetValue(person2, out relationshipLevel);
+
+                if (relationshipLevel == 3) // Уровень отношений - семья.
+                {
+                    Console.WriteLine($"{person1.FirstName} и {person2.FirstName} уже в браке");
+                }
+                else
+                {
+                    EditRelationships(person1, person2, 3);
+                    Console.WriteLine($"{person1.FirstName} и {person2.FirstName} заключают брак");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создание отношений.
+        /// </summary>
+        /// <param name="person1">Первый персонаж.</param>
+        /// <param name="person2">Второй персонаж.</param>
+        /// <param name="relationshipLevel">Уровень отношений (0 - знакомые, 1 - друзья, 2 - любовники, 3 - семья.</param>
+        private static void BuildingRelationships(Person person1, Person person2, uint relationshipLevel)
+        {
+            person1.Relationship.Add(person2, relationshipLevel);
+            person2.Relationship.Add(person1, relationshipLevel);
+        }
+
+        /// <summary>
+        /// Изменение отношений.
+        /// </summary>
+        /// <param name="person1">Первый персонаж.</param>
+        /// <param name="person2">Второй персонаж.</param>
+        /// <param name="relationshipLevel">Уровень отношений (0 - знакомые, 1 - друзья, 2 - любовники, 3 - семья.</param>
+        private static void EditRelationships(Person person1, Person person2, uint relationshipLevel)
+        {
+            person1.Relationship[person2] = relationshipLevel;
+            person2.Relationship[person1] = relationshipLevel;
+        }
+    }
+}
---
+++ Urleid.Remember/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urleid.Remember
+{
+    class Person
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        // Уровень отношений: 0 - знакомые, 1 - друзья, 2 - любовники, 3 - семья.
+        private Dictionary<Person, uint> _relationship;
+        public Dictionary<Person, uint> Relationship
+        {
+            get { return _relationship; }
+            set { _relationship = value; }
+        }
+
+
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+
+            _relationship = new Dictionary<Person, uint>();
+        }
+    }
+}
